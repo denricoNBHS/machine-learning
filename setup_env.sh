d91b114b9080a5bf0fa0927a2fcf297759e5f412
Dave Enrico <denrico@northbergen.k12.nj.us>
2017-04-05 08:29:35 -0400

    Change setup_py to create stem env instead of testing

--- setup_env.sh
+++ setup_env.sh
@@ -1,4 +1,4 @@
-yes | conda create -n testing python=3.5 matplotlib jupyter scikit-learn seaborn
-source activate testing 
+yes | conda create -n stem python=3.5 matplotlib jupyter scikit-learn seaborn
+source activate stem
 value=$(getconf LONG_BIT)
 if [ $value = 64 ]; then pip install tensorflow; fi
